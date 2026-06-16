a764b6234cf6c1349d5ffa328d4aa4031f34636b
root <root@DESKTOP-SL3RMAO.localdomain>
2026-06-16 13:44:00 +0300

    Исправлен бэкенд Object Storage

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,10 +9,12 @@ terraform {
   }
 
   backend "s3" {
-    endpoint   = "storage.yandexcloud.net"
-    bucket     = "vkr-tfstate"
-    key        = "terraform.tfstate"
-    region     = "ru-central1"
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+    bucket = "vkr-tfstate"
+    key    = "terraform.tfstate"
+    region = "ru-central1"
 
     skip_region_validation      = true
     skip_credentials_validation = true
